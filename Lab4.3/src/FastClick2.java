78f714f40a00ec1c66ab13d9611cf69960e8d73c
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2018-03-27 09:21:52 -0400

    XD

---
+++ Lab4.3/src/FastClick2.java
@@ -0,0 +1,109 @@
+
+	
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+
+public class FastClick2 extends Application {
+	long timeStep = 0 ; 
+	@Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("2Fast2Click");
+      
+        
+        
+        
+        
+        GridPane grid = new GridPane() ; 
+        grid.setAlignment(Pos.CENTER) ;
+        grid.setHgap(10) ; 
+        grid.setVgap(10) ; 
+        grid.setPadding(new Insets(25,25,25,25));
+        
+        Text scenetitle = new Text("CLICK ME HARDER DADDY");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL,20)) ; 
+        grid.add(scenetitle,0,0,2,1); 
+      
+        
+        Button btn = new Button("ME");
+      //  btnDelete.setMinWidth(80.0);
+       // btnDelete.setPrefWidth(80.0);
+       // btnDelete.setMaxWidth(80.0);
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.CENTER);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4); 
+        
+        timeStep = System.nanoTime() + 10000L ; 
+        new AnimationTimer() 
+        {
+        	public void handle(long now)
+        	{
+        		if (now > timeStep) 
+        		{
+        			timeStep = now + 10000L;
+        			//scoring = !scoring;
+        		}
+        	/*	if (!scoring)
+        		{
+        			btn.setText("Scammer");
+        		}*/
+        		else {
+        			btn.setText("STOP");
+        		}
+        	//	txt.setText("Score:" + Integer.toString(score));
+        	}
+        } ;
+      
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+        
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+        	int score = 0;  boolean scoring = true ;  
+            @Override
+            public void handle(ActionEvent e) {
+            	
+            	if (scoring) {
+            		score++;
+            	}
+            	else {
+            		score+=0; 
+            	}
+            
+                actiontarget.setFill(Color.BLACK) ;
+                actiontarget.setText("Score: " + score + "\nTIMELEFT:" + timeStep) ;
+                actiontarget.setFont(Font.font("Tahoma", FontWeight.NORMAL,20)); 
+                System.out.println(score);
+            }
+        });
+        
+        Scene S1 = new Scene (grid,500,500);
+        primaryStage.setScene(S1);
+        
+        
+        
+        
+        primaryStage.show();
+    }
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
--- Lab4.3/src/FastClicker.java
+++ Lab4.3/src/FastClicker.java
@@ -1,5 +1,6 @@
 
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -11,13 +12,13 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
- 
+
 
 public class FastClicker extends Application { //JavaFX application defines the user interface container by means of a stage and a scene. 
     public static void main(String[] args) {
         launch(args);
     }
-    
+    long timeStep = 0 ; 
     @Override
     public void start(Stage primaryStage) { //The JavaFX Stage class is the top-level JavaFX container.
         primaryStage.setTitle("CLICK FASTER!");
@@ -36,19 +37,41 @@ public class FastClicker extends Application { //JavaFX application defines the 
             } 
         });
         
-        timeStep = System.nanoTime() + 1000000000L ; 
-        new AnimationTimer() {
-        	
-        }
        
-    
+        
+        timeStep = System.nanoTime() + 10000000L ; 
+        new AnimationTimer() 
+        {
+        	public void handle(long now)
+        	{
+        		if (now > timeStep) 
+        		{
+        			timeStep = now + 10000000L;
+        			//scoring = !scoring;
+        		}
+        	/*	if (!scoring)
+        		{
+        			btn.setText("Scammer");
+        		}*/
+        		else {
+        			btn.setText("STOP");
+        		}
+        	//	txt.setText("Score:" + Integer.toString(score));
+        	}
+        } ;
+        	
+        Button btn2 = new Button("Sign in");
+        HBox hbBtn2 = new HBox(10);
+        hbBtn2.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn2.getChildren().add(btn);
+      //  grid.add(hbBtn2, 1, 4); 
         
 
         
         StackPane root = new StackPane(); //this example, the root node is a StackPane object, which is a resizable layout node.
         root.getChildren().add(btn); //child node, a button control with text, plus an event handler to print a message when the button is pressed.
         
-        primaryStage.setScene(new Scene(root, 200, 200)); //The JavaFX Scene class is the container for all content.
+        primaryStage.setScene(new Scene(root, 400, 400)); //The JavaFX Scene class is the container for all content.
         primaryStage.show();
     }
     
